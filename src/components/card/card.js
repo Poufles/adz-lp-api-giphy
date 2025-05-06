4d6300271a5d5b7312f82a20ea7526571c2839ed
Evan Ayalin <siyeel04@gmail.com>
2025-05-06 10:37:36 +0800

    Added basic Card functionalities.

---
+++ src/components/card/card.js
@@ -0,0 +1,76 @@
+export default function Card(object) {
+
+    let
+    source, 
+    title = 'No Title', 
+    profile_link = 'https://giphy.com/', 
+    display_name = 'N/A';
+
+    source = object.images.original.url;
+    
+    if (object.title) {
+        title = object.title;
+    };
+    
+    if (object.user && object.user.profile_url) {
+        profile_link = object.user.profile_url;
+    };
+
+    if (object.user && object.user.display_name) {
+        display_name = object.user.display_name;
+    };
+
+    console.log(source);
+    console.log(title);
+    console.log(profile_link);
+    console.log(display_name);
+
+    const template = `
+        <article class="component card">
+        <picture>
+            <img src="${source}" alt="" srcset="">
+            <div class="overlay"></div>
+            <a target="_blank" href="https://giphy.com/" class="giphy">
+                <svg height="2500" width="2500" xmlns="http://www.w3.org/2000/svg" viewBox="4 2 16.32 20"><g fill="none" fill-rule="evenodd"><path d="M6.331 4.286H17.99v15.428H6.33z" fill="#000"/><g fill-rule="nonzero"><path d="M4 3.714h2.331v16.572H4z" fill="#04ff8e"/><path d="M17.989 8.286h2.331v12h-2.331z" fill="#8e2eff"/><path d="M4 19.714h16.32V22H4z" fill="#00c5ff"/><path d="M4 2h9.326v2.286H4z" fill="#fff152"/><path d="M17.989 6.571V4.286h-2.332V2h-2.331v6.857h6.994V6.571" fill="#ff5b5b"/><path d="M17.989 11.143V8.857h2.331" fill="#551c99"/></g><path d="M13.326 2v2.286h-2.332" fill="#999131"/></g></svg>
+            </a>
+        </picture>
+        <div class="description text no-select no-u-select">
+            <p class="name">
+                ${title}
+            </p>
+            <p class="author">
+                by <a target="_blank" href="${profile_link}">${display_name}</a>
+            </p>
+        </div>
+    </article>
+    `;
+
+    const range = document.createRange();
+    const fragment = range.createContextualFragment(template);
+    const component = fragment.querySelector('.component');
+    const gif_img = component.querySelector('img.gif');
+    const gif_name = component.querySelector('.name');
+    const gif_author = component.querySelector('.author');
+    const gif_author_link = gif_author.querySelector('a');
+
+    return {
+
+        /**
+         * Renders the component.
+         * @param {HTMLElement} parent - To where the component must be appended.
+         * @returns The component if no parent.
+         */
+        render:  function(parent) {
+            if (!parent) return component;
+    
+            if (!parent.contains(component)) {
+                parent.appendChild(component);
+            };
+        },
+    
+        unrender: function() {
+    
+        }
+
+    };
+};
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,33 +1,36 @@
 import '../styles/main.css';
 import '../styles/app.css';
+import '../components/card/card.css';
+
+import Card from '../components/card/card.js';
 
 function App() {
-    
+
     /*
     * I tried using dotenv-webpack to know how API keys
      * should be hidden but ofcourse, there is no backend
      * therefore, voila ! The GIPHY key !   
     */
-    const api_key = process.env.GIPHY_API_KEY; 
-    const body = document.body;
+    const api_key = process.env.GIPHY_API_KEY;
     const cont_left = document.getElementById('left');
     const cont_right = document.getElementById('right');
 
-    const main = cont_right.querySelector('main#content'); 
-    
-    // fetch(`https://api.giphy.com/v1/gifs/search?api_key=${api_key}&limit=5&q=lebron james`, { mode: 'cors' })
+    const main = cont_right.querySelector('main#content');
+
+
+    // fetch(`https://api.giphy.com/v1/gifs/search?api_key=${api_key}&limit=6&q=lebron james`, { mode: 'cors' })
     //     .then(function (response) {
     //         return response.json();
     //     })
     //     .then(function (response) {
+    //         console.log(response);
     //         const data = response.data
-            
+    //         console.log(response);
+    //         console.log(data);
+
     //         for (let item of data) {
-    //             const url = item.images.original.url;
-    //             const img = document.createElement('img');
-                
-    //             img.src = url;
-    //             main.appendChild(img);
+    //             const gif = Card(item);
+    //             gif.render(main);
     //         };
 
     //     });
